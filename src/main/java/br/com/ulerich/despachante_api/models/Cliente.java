a2a6b0602c51b4dd5599bf42c14a864d2f22ad6d
Luan-Ulerich <luanulerich7@gmail.com>
2026-04-02 19:08:43 -0300

    Commit inicial: Estrutura base do Spring Boot

---
+++ src/main/java/br/com/ulerich/despachante_api/models/Cliente.java
@@ -0,0 +1,57 @@
+package br.com.ulerich.despachante_api.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_cliente;
+
+    private String nome;
+    private String cpf_cnpj;
+    private String telefone;
+
+
+    public Cliente() {
+    }
+
+
+    public Long getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(Long id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf_cnpj() {
+        return cpf_cnpj;
+    }
+
+    public void setCpf_cnpj(String cpf_cnpj) {
+        this.cpf_cnpj = cpf_cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
---
+++ src/test/java/br/com/ulerich/despachante_api/DespachanteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ulerich.despachante_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespachanteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
